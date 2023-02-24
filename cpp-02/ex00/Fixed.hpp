83f386a1f0a43524dbeef80c823022e4e49bfb08
o7ab <omaremad951@gmail.com>
2023-02-25 01:33:50 +0400

    finished ex00 in cpp02

---
+++ cpp-02/ex00/Fixed.hpp
@@ -0,0 +1,20 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed
+{
+	private:
+		int fixedPointValue;
+		static const int fractionalBits = 8;
+	public:
+		Fixed();
+		Fixed(Fixed const & src);
+		~Fixed();
+		Fixed & operator=(Fixed const & rhs);
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+};
+
+# endif
